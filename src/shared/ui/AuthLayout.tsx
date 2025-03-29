e7d55936b38bfbd5d4e0b5b96f43d92b3a861258
Goddo-ro <r0manovic1@yandex.ru>
2025-03-29 21:32:12 +0300

    close button and wide screen adaptation to auth layout

--- src/shared/ui/AuthLayout.tsx
+++ src/shared/ui/AuthLayout.tsx
@@ -2,7 +2,10 @@ import { PropsWithChildren, ReactElement } from "react";
 import { useNavigate } from "react-router";
 
 import { Back } from "@/shared/icons/Back";
+import { Close } from "@/shared/icons/Close";
 import { Title } from "@/shared/icons/Title";
+import { cn } from "@/shared/lib/utils";
+import { routes } from "@/shared/router";
 import { Button } from "@/shared/ui/button";
 
 interface AuthLayout extends PropsWithChildren {
@@ -19,7 +22,12 @@ export const AuthLayout = ({
 
   return (
     <div className="h-svh w-screen flex items-center justify-center">
-      <div className="h-full max-w-3xl flex flex-col justify-between items-center p-5 pt-20 relative">
+      <div
+        className={cn(
+          "relative h-full max-w-3xl flex flex-col gap-5 justify-between items-center p-5 pt-[20%]",
+          "md:pt-5 md:max-h-[600px] md:background-red-100 md:bg-white md:rounded-xl md:shadow-md",
+        )}
+      >
         {withBackBtn && (
           <Button
             className="absolute left-5 top-5"
@@ -28,9 +36,18 @@ export const AuthLayout = ({
             }}
             variant="ghost"
           >
-            <Back />
+            <Back className="size-[20px]" />
           </Button>
         )}
+        <Button
+          className="absolute right-5 top-5"
+          variant="ghost"
+          onClick={() => {
+            navigate(routes.main, { replace: true });
+          }}
+        >
+          <Close color="#4B6A51" className="size-[20px]" />
+        </Button>
         <header>
           <Title />
         </header>
