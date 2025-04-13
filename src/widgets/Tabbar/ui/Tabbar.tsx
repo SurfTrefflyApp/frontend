daea9bb084636136572c5d064a648eb1cd090978
Goddo-ro <r0manovic1@yandex.ru>
2025-04-13 16:40:38 +0300

    change tabbar size

--- src/widgets/Tabbar/ui/Tabbar.tsx
+++ src/widgets/Tabbar/ui/Tabbar.tsx
@@ -5,18 +5,18 @@ import { routes } from "@/shared/router";
 
 export const Tabbar = () => {
   return (
-    <nav className="w-full py-4 px-10 bg-[#F4F4F0] rounded-t-xl flex justify-between">
+    <nav className="w-full py-2 px-10 bg-[#F4F4F0] rounded-t-xl flex justify-between shadow-3xl z-10">
       <Link to={routes.main}>
-        <House className="text-primary w-[40px] h-[40px]" />
+        <House className="text-primary w-[36px] h-[36px]" />
       </Link>
       <Link to={routes.eventsSearch}>
-        <Map className="text-primary w-[40px] h-[40px]" />
+        <Map className="text-primary w-[36px] h-[36px]" />
       </Link>
       <Link to={routes.events}>
-        <CalendarRange className="text-primary w-[40px] h-[40px]" />
+        <CalendarRange className="text-primary w-[36px] h-[36px]" />
       </Link>
       <Link to={routes.profile}>
-        <User className="text-primary w-[40px] h-[40px]" />
+        <User className="text-primary w-[36px] h-[36px]" />
       </Link>
     </nav>
   );
