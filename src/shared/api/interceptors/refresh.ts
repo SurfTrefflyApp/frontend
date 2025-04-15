8776965ecaebb5da83d65176b0a020922beae7a0
Goddo-ro <r0manovic1@yandex.ru>
2025-04-15 22:42:02 +0300

    add failed requests queue for after refresh retries

--- src/shared/api/interceptors/refresh.ts
+++ src/shared/api/interceptors/refresh.ts
@@ -6,27 +6,50 @@ import { refresh } from "@/shared/auth/api";
 
 export class RefreshInterceptor {
   private isRefreshing = false;
+  private failedQueue: {
+    resolve: (value?: unknown) => void;
+    reject: (error: unknown) => void;
+    request: AxiosRequestConfig;
+  }[] = [];
   private axiosInstance: AxiosInstance;
 
   constructor(axiosInstance: AxiosInstance) {
     this.axiosInstance = axiosInstance;
   }
 
-  public async refresh(error: unknown, originalRequest: AxiosRequestConfig) {
-    if (!this.isRefreshing) {
-      try {
-        this.isRefreshing = true;
-        await refresh();
-        return this.axiosInstance(originalRequest);
-      } catch (refreshError) {
-        logoutWithoutApiEvent();
-        setErrorCodeEvent(401);
-        setErrorEvent(refreshError);
-        return Promise.reject(refreshError);
-      } finally {
-        this.isRefreshing = false;
+  private processQueue(error: unknown, tokenRefreshed = false) {
+    this.failedQueue.forEach(({ resolve, reject, request }) => {
+      if (tokenRefreshed) {
+        resolve(this.axiosInstance(request));
+      } else {
+        reject(error);
       }
-    }
-    return Promise.reject(error);
+    });
+
+    this.failedQueue = [];
+  }
+
+  public refresh(_: unknown, originalRequest: AxiosRequestConfig) {
+    return new Promise((resolve, reject) => {
+      this.failedQueue.push({ resolve, reject, request: originalRequest });
+
+      if (!this.isRefreshing) {
+        this.isRefreshing = true;
+
+        refresh()
+          .then(() => {
+            this.processQueue(null, true);
+          })
+          .catch((refreshError) => {
+            logoutWithoutApiEvent();
+            setErrorCodeEvent(401);
+            setErrorEvent(refreshError);
+            this.processQueue(refreshError, false);
+          })
+          .finally(() => {
+            this.isRefreshing = false;
+          });
+      }
+    });
   }
 }
