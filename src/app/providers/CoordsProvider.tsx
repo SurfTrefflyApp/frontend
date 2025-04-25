2f58ea79b9082f7c4e68c322c758936faeaef0cd
Goddo-ro <r0manovic1@yandex.ru>
2025-04-26 00:31:27 +0300

    remove chore console debug

--- src/app/providers/CoordsProvider.tsx
+++ src/app/providers/CoordsProvider.tsx
@@ -6,8 +6,6 @@ import useGeolocation from "@/shared/coords/useGeolocation";
 export const CoordsProvider = () => {
   const state = useGeolocation();
 
-  console.debug(state);
-
   return (
     <CoordsContext.Provider value={state}>
       <Outlet />
