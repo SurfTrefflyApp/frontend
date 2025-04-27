84646abaf072e1d038e0cdcea8691f4cce93dd2e
Goddo-ro <r0manovic1@yandex.ru>
2025-04-28 01:29:44 +0300

    make incorrect symbolds validation first priority

--- src/pages/Register/model/formSchema.ts
+++ src/pages/Register/model/formSchema.ts
@@ -4,20 +4,24 @@ import { z } from "zod";
 export const formSchema = z.object({
   email: z
     .string()
-    .min(1, { message: "Поле не может быть пустым" })
     .refine((email) => {
       const regex = /[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+/u;
       return regex.test(email) && !emojiRegex().test(email);
-    }, "Некорректный формат почты"),
+    }, "Некорректный формат почты")
+    .pipe(z.string().min(1, { message: "Поле не может быть пустым" })),
   username: z
     .string()
-    .min(1, { message: "Поле не может быть пустым" })
-    .min(2, { message: "Длина ввода должна быть от 2 до 20 символов" })
-    .max(20, { message: "Длина ввода должна быть от 2 до 20 символов" })
     .refine((username) => {
       const regex = /^[\p{L}-]+$/u;
       return regex.test(username) && !emojiRegex().test(username);
-    }, "Введены некорректные символы"),
+    }, "Введены некорректные символы")
+    .pipe(
+      z
+        .string()
+        .min(1, { message: "Поле не может быть пустым" })
+        .min(2, { message: "Длина ввода должна быть от 2 до 20 символов" })
+        .max(20, { message: "Длина ввода должна быть от 2 до 20 символов" }),
+    ),
   password: z
     .string()
     .refine(
